83135ed6ac071ab34e4eafd398c284d4b3393e9d
Kevin Washington <kevin.washingtonbr@gmail.com>
2020-05-14 14:05:05 -0300

    bar updates

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h3>{{ title }}</h3>
-    <b-progress :max="available" height="2rem" variant="info">
+    <b-progress
+      :max="available"
+      class="horizontal-bar"
+      height="2rem"
+      variant="info"
+    >
       <b-progress-bar :value="used">
         <strong>{{ calculatePercentage }} ocupados</strong>
         <span class="mt-0">{{ used + "/" + available }}</span>
@@ -15,13 +20,13 @@ export default {
   props: ["chartdata"],
   computed: {
     title() {
-      return this.chartdata.datasets[0].title;
+      return this.chartdata.title;
     },
     available() {
-      return this.chartdata.datasets[0].data[1];
+      return this.chartdata.data[1];
     },
     used() {
-      return this.chartdata.datasets[0].data[0];
+      return this.chartdata.data[0];
     },
     calculatePercentage() {
       return ((this.used / this.available) * 100).toFixed(0) + "%";
@@ -34,5 +39,8 @@ h3 {
   font-size: 1rem;
   font-weight: 500;
   line-height: 1.2;
+}
+.progress-bar {
+  background: #9d5cb6 !important;
 }
 </style>
