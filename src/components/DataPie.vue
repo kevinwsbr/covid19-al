dc022d0eaa78d1543ef2d1f1e60861987687faed
Kevin Washington <kevin.washingtonbr@gmail.com>
2020-05-14 14:05:15 -0300

    dpie updates

--- src/components/DataPie.vue
+++ src/components/DataPie.vue
@@ -4,11 +4,29 @@ import { Doughnut } from "vue-chartjs";
 export default {
   extends: Doughnut,
   props: ["chartdata"],
+  computed: {
+    chartContent() {
+      return {
+        labels: ["Utilizados", "Disponíveis"],
+        datasets: [
+          {
+            backgroundColor: ["#9d5cb6", "#e9ecef"],
+            data: [
+              this.chartdata.data[0],
+              this.chartdata.data[1] - this.chartdata.data[0],
+            ],
+            title: this.chartdata.title,
+            borderWidth: 1,
+          },
+        ],
+      };
+    },
+  },
   data: () => ({
     options: {
       title: {
         display: true,
-        text: "",
+        text: "Leitos disponíveis",
         fontSize: 14,
         padding: 8,
         fontFamily: '"Open Sans", sans-serif',
@@ -26,11 +44,7 @@ export default {
     },
   }),
   mounted() {
-    this.options.title.text = this.chartdata.datasets[0].title;
-
-    this.chartdata.datasets[0].data[1] -= this.chartdata.datasets[0].data[0];
-
-    this.renderChart(this.chartdata, this.options);
+    if (this.chartdata) this.renderChart(this.chartContent, this.options);
   },
 };
 </script>
