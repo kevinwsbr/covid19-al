3f21381b333d410b8568dabc16665e6d6e8e36f3
Kevin Washington <kevin.washingtonbr@gmail.com>
2020-05-15 11:31:37 -0300

    pie updates

--- src/components/cards/PieChartCard.vue
+++ src/components/cards/PieChartCard.vue
@@ -1,12 +1,12 @@
 <template>
   <b-card class="pie-card" header-tag="header">
     <template v-slot:header>
-      <div class="d-flex justify-content-between">
-        <div class="my-auto w-100">
-          <h2 class="mb-0">Leitos reservados para COVID-19</h2>
+        <div class="d-flex justify-content-between">
+          <div class="my-auto w-100">
+            <h2 class=" mb-0">Leitos reservados para COVID-19</h2>
+          </div>
         </div>
-      </div>
-    </template>
+      </template>
     <b-row class="mx-2 mb-4">
       <b-col v-if="total" class="py-1 my-2 inner-pie-card" cols="12" md="4">
         <DataPie :chartdata="total" />
@@ -19,11 +19,6 @@
           </b-row>
         </div>
       </b-col>
-      <!-- <b-col>
-        <b-row v-for="i in 3" :key="i">
-          <b-col><BarChart /></b-col>
-        </b-row>
-      </b-col> -->
     </b-row>
   </b-card>
 </template>
@@ -47,11 +42,7 @@ export default {
 </script>
 
 <style lang="scss">
-h2 {
-  font-size: 1.2rem;
-  font-weight: 600;
-  letter-spacing: -0.05rem;
-}
+
 .card {
   border-radius: 1rem;
   box-shadow: 0 4px 6px 0 rgba(31, 70, 88, 0.04);
@@ -69,91 +60,9 @@ h2 {
   padding: 0;
 }
 
-.map-alert {
-  height: 400px;
-  color: #fff;
-  background: rgba(0, 0, 0, 0.4);
-  border-bottom-left-radius: 1rem;
-  border-bottom-right-radius: 1rem;
-  z-index: 5;
-  width: 100%;
-  position: absolute;
 
-  span {
-    line-height: 400px;
-    font-size: 1.2em;
-    font-weight: 600;
-  }
-}
-
-.leaflet-container {
-  z-index: 3;
-  height: 400px !important;
-  border-bottom-left-radius: 1rem;
-  border-bottom-right-radius: 1rem;
-}
-
-.btn-confirmed,
-.btn-deaths {
-  font-weight: 900 !important;
-}
-
-@media screen and (max-width: 576px) {
-  .leaflet-container {
-    height: 360px !important;
-  }
-  .btn-confirmed,
-  .btn-deaths {
-    float: right;
-  }
-
-  .map-alert {
-    height: 360px;
-
-    span {
-      line-height: 360px;
-      font-size: 0.9rem;
-    }
-  }
-}
-
-.btn-confirmed {
-  background: #f49e39 !important;
-  border-color: #e09137 !important;
-  font-size: 0.75rem !important;
-  &:focus {
-    box-shadow: 0 0 0 0.2rem rgba(244, 158, 57, 0.5) !important;
-  }
-}
-
-.btn-deaths {
-  background: #3597db !important;
-  border-color: #328ccc !important;
-  font-size: 0.75rem !important;
-  &:focus {
-    box-shadow: 0 0 0 0.2rem rgba(53, 151, 219, 0.5) !important;
-  }
-}
 </style>
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap");
-.card-map {
-  padding: 0;
-}
-* {
-  font-family: "Open Sans", sans-serif;
-}
-h2 {
-  font-size: 1.2rem;
-  font-weight: 600;
-  letter-spacing: -0.05rem;
-}
-.card {
-  border-radius: 1rem;
-  box-shadow: 0 4px 6px 0 rgba(31, 70, 88, 0.04);
-  background: #fff;
-  border: 1px solid #dbe9f5;
-}
 
 @media screen and (max-width: 1024px) {
   .inner-pie-card {
