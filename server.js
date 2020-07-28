0154626be893a42a98a8bbf24da268cd077252d4
Kevin Washington <kevin.washingtonbr@gmail.com>
2020-07-27 21:00:36 -0300

    force https

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const configs = {
 
 if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
     app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-        if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP
-            res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
-        else //Se a requisição já é HTTPS
-            next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+        //console.log(JSON.stringify(req.headers))
+        //console.log(req.headers["x-forwarded-proto"])
+        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
+        //if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP
+        //else //Se a requisição já é HTTPS
+        //    next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
     });
 
 app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
